ac9980112c4078e20dba6349115b53caaf850137
Taiã Pryor <taiabpryor@gmail.com>
2017-06-16 14:57:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-$(document).ready( function() {
-	
-	// Accordion 
-	// function myAccFunc() {
-		// var x = document.getElementById("demoAcc");
-		// if (x.className.indexOf("w3-show") == -1) {
-			// x.className += " w3-show";
-		// } else {
-			// x.className = x.className.replace(" w3-show", "");
-		// } 
-	// }
+$(document).ready( function() {	
 
-	// Click on the "Jeans" link on page load to open the accordion for demo purposes
-	// document.getElementById("myBtn").click();
-
-
-	// Script to open and close sidebar
+	//Open sidebar
 	function openMenu() {
 		document.getElementById("mySidebar").style.display = "block";
 		document.getElementById("myOverlay").style.display = "block";
 	}
-
+	//Close sidebar
 	function closeMenu() {
 		document.getElementById("mySidebar").style.display = "none";
 		document.getElementById("myOverlay").style.display = "none";
@@ -32,14 +18,18 @@ $(document).ready( function() {
 	$('.closeMenu').click( function(){
 		closeMenu();
 	});
-	/*
-	function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.9";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-	*/
+	
+	$('.closeMenu').click( function(){
+		closeMenu();
+	});
+
+	// Facebook plugin code
+	(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.9";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));	
 	
 });
